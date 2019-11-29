a365450ae3d374ab1c5412b9192a1fd73da5b692
spaceface777 <spaceface7777@gmail.com>
2019-11-29 16:33:36 +0100

    Massive improvements

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # JSXLite
 Use JSX to create DOM elements without importing a huge library like React
 
- - VERY small footprint (~250 bytes minified + gzipped)
+ - VERY small footprint (<350 bytes minified + gzipped)
  - 0 dependencies
  - TypeScript support (typings included with library)
 
+## Features
+
+**This library does not aim to be another React-like framework, and therefore many of React's core features have been completely left out. Instead, it aims to be a lightweight tool for those people (like me) who want to use JSX to create DOM elements, but don't want to install a huge, bloated library to do so. If you want a full web framework, this library is probably not meant for you.**
+
+
+#### Features included
+- Using JSX to create elements
+- Functional components
+    - Passing custom props to these components
+- HTML attributes, props...
+- A complete, well-tested TypeScript type definition
+
+#### Features not included
+- A virtual DOM implementation
+    - A component's internal state
+- Class components (useless if state isn't implemented)
+
+
 ## Usage
-#### TypeScript
 1. Install the library
     ```sh
     npm install jsxlite
     ```
 
-2. Add the following to your `tsconfig.json`: 
+2. *(TypeScript)* Add the following to your `tsconfig.json`: 
     ```json
     "compilerOptions": {
         "jsx": "react",
@@ -20,7 +37,7 @@ Use JSX to create DOM elements without importing a huge library like React
     }
     ```
 
-3. Import the library at the top of whichever `.tsx` file you want to use it in:
+3. Import the library at the top of whichever `.jsx` or `.tsx` file you want to use it in:
     ```ts
     import JSX from "jsxlite"
     ```
@@ -32,3 +49,36 @@ Use JSX to create DOM elements without importing a huge library like React
     document.body.appendChild(<span id="example" onclick="alert('Hello')">Hi there</span>)
     ```
 
+## Function components
+Function components are supported by the library, which means that you can create functional components and pass props to them like you would in React.
+
+***Note: The function name must start with an uppercase letter, as this is how a function component is differentiated from a regular HTML element.***
+
+```tsx
+interface ExampleProps {
+    name: string;
+    version: string;
+}
+
+function Example (props: ExampleProps) {
+    const features = [
+        "lightweight",
+        "easy to use",
+        "simply amazing!"
+    ]
+
+    return (
+        <div class="example">
+            <p>Hello there!</p>
+            <p>This is the {props.name} library, version v{props.version}</p>
+            <p>It has many great features:</p>
+            <ol>
+                {features.map(f => <li>{'It is ' + f}</li>)}
+            </ol>
+        </div>
+    )
+}
+
+document.body.appendChild(<Example version="2.0.0" name="JSXLite"/>)
+
+```
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,61 @@
-type Props = { [key: string]: string }
-type Children = (string | Element)[]
+type ElementName = 
+  | string
+  | ((props: any) => HTMLElement) // Function components
 
-// Actual processing function - converts JSX syntax to real DOM elements
+interface PropsArg {
+  [prop: string]: string | (() => any)
+}
+
+
+type Child = (string | HTMLElement)
+
+type Children = Child[] | Child[][]
+
+
 export const JSX = {
-  createElement (name: string, props: Props, ...children: Children): HTMLElement {
-    const element = document.createElement(name)
-    props = props || {}
+  // Core function of the module - parses JSX into DOM elements
+  // It's called automatically when transforming JSX - 
+  // it is not meant to be called directly by the user
+  createElement (name: ElementName, props: PropsArg, ...children: Children): HTMLElement {
+    props = props || {} // Prevent null from being passed
 
-    for (const prop in props) {
-      switch (prop.toLowerCase()) {
-        case 'classname': element.setAttribute('class', props[prop]) // Allow React-like 'className' attribute
-        case 'innerhtml': element.innerHTML = props[prop] // Allow setting the element's inner HTML
-        default: element.setAttribute(prop, props[prop])
+    // Allow for function components
+    if (typeof name === 'function') return name(props)
+
+    const element = document.createElement(name)
+
+    for (const prop of Object.keys(props)) {
+      const val = props[prop]
+
+      if (typeof val === 'string') {
+        switch (prop.toLowerCase()) {
+          case 'classname': element.setAttribute('class', val) // Allow React-like 'className' attribute
+          case 'innerhtml': element.innerHTML = val // Allow setting the element's inner HTML
+          default: element.setAttribute(prop, val) // If not, simply pass the prop to the element
+        }
+      } else {
+        const match = prop.match(/on(\w+)/)
+        const event = match?.[1]
+        if (event) {
+          element.addEventListener(event, val)
+        }
       }
     }
 
-    for (const child of children) {
+    function createChild (child: Child) {
       if (typeof child === 'string') {
         element.appendChild(document.createTextNode(child))
       } else {
         element.appendChild(child)
       }
+    }
+
+    for (const child of children) {
+      if (Array.isArray(child)) {
+        child.forEach(c => createChild(c)) // Allow property arrays
+      }
+
+      else createChild(child)
     }
 
     return element
@@ -29,12 +64,120 @@ export const JSX = {
 
 export default JSX
 
-// TypeScript definitions for the module
+// Typescript types
 declare global {
   namespace JSX {
     type Element = HTMLElement;
-    interface IntrinsicElements {
-      [tag: string]: any;
-    }
+
+    type IntrinsicElements = {
+      // HTMLElementTagNameMap = every HTML tag
+      [i in keyof HTMLElementTagNameMap]: Props;
+    };
   }
+}
+
+export interface Props extends EventListeners {
+  id?: string;
+  class?: string;
+  className?: string;
+  innerHTML?: string;
+  [prop: string]: any;
+}
+
+// Event listeners
+export interface EventListeners {
+  oncopy?: (e: ClipboardEvent) => any
+  oncut?: (e: ClipboardEvent) => any
+  onpaste?: (e: ClipboardEvent) => any
+
+  oncompositionend?: (e: CompositionEvent) => any
+  oncompositionstart?: (e: CompositionEvent) => any
+  oncompositionupdate?: (e: CompositionEvent) => any
+
+  onfocus?: (e: FocusEvent) => any
+  onblur?: (e: FocusEvent) => any
+
+  onchange?: (e: Event) => any
+  onbeforeinput?: (e: Event) => any
+  oninput?: (e: Event) => any
+  onreset?: (e: Event) => any
+  onsubmit?: (e: Event) => any
+  oninvalid?: (e: Event) => any
+
+  onload?: (e: Event) => any
+  onerror?: (e: Event) => any // also a Media Event
+
+  onkeydown?: (e: KeyboardEvent) => any
+  onkeypress?: (e: KeyboardEvent) => any
+  onkeyup?: (e: KeyboardEvent) => any
+
+  onabort?: (e: Event) => any
+  oncanplay?: (e: Event) => any
+  oncanplaythrough?: (e: Event) => any
+  ondurationchange?: (e: Event) => any
+  onemptied?: (e: Event) => any
+  onencrypted?: (e: Event) => any
+  onended?: (e: Event) => any
+  onloadeddata?: (e: Event) => any
+  onloadedmetadata?: (e: Event) => any
+  onloadstart?: (e: Event) => any
+  onpause?: (e: Event) => any
+  onplay?: (e: Event) => any
+  onplaying?: (e: Event) => any
+  onprogress?: (e: Event) => any
+  onratechange?: (e: Event) => any
+  onseeked?: (e: Event) => any
+  onseeking?: (e: Event) => any
+  onstalled?: (e: Event) => any
+  onsuspend?: (e: Event) => any
+  ontimeupdate?: (e: Event) => any
+  onvolumechange?: (e: Event) => any
+  onwaiting?: (e: Event) => any
+
+  // MouseEvents
+  onauxclick?: (e: MouseEvent) => any
+  onclick?: (e: MouseEvent) => any
+  oncontextmenu?: (e: MouseEvent) => any
+  ondoubleclick?: (e: MouseEvent) => any
+  ondrag?: (e: DragEvent) => any
+  ondragend?: (e: DragEvent) => any
+  ondragenter?: (e: DragEvent) => any
+  ondragexit?: (e: DragEvent) => any
+  ondragleave?: (e: DragEvent) => any
+  ondragover?: (e: DragEvent) => any
+  ondragstart?: (e: DragEvent) => any
+  ondrop?: (e: DragEvent) => any
+  onmousedown?: (e: MouseEvent) => any
+  onmouseenter?: (e: MouseEvent) => any
+  onmouseleave?: (e: MouseEvent) => any
+  onmousemove?: (e: MouseEvent) => any
+  onmouseout?: (e: MouseEvent) => any
+  onmouseover?: (e: MouseEvent) => any
+  onmouseup?: (e: MouseEvent) => any
+
+  onselect?: (e: Event) => any
+
+  ontouchcancel?: (e: TouchEvent) => any
+  ontouchend?: (e: TouchEvent) => any
+  ontouchmove?: (e: TouchEvent) => any
+  ontouchstart?: (e: TouchEvent) => any
+
+  onpointerdown?: (e: PointerEvent) => any
+  onpointermove?: (e: PointerEvent) => any
+  onpointerup?: (e: PointerEvent) => any
+  onpointercancel?: (e: PointerEvent) => any
+  onpointerenter?: (e: PointerEvent) => any
+  onpointerleave?: (e: PointerEvent) => any
+  onpointerover?: (e: PointerEvent) => any
+  onpointerout?: (e: PointerEvent) => any
+
+  onscroll?: (e: UIEvent) => any
+
+  onwheel?: (e: WheelEvent) => any
+
+  onanimationstart?: (e: AnimationEvent) => any
+  onanimationend?: (e: AnimationEvent) => any
+  onanimationiteration?: (e: AnimationEvent) => any
+
+  ontransitionend?: (e: TransitionEvent) => any
 }
